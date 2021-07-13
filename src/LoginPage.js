4e09a458079ee4a0c9210bbbb38fba3c29073701
Shohrukh Jalolov <sjalolov@lclark.edu>
2021-07-12 17:30:55 -0700

    Add new tools

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -17,7 +17,7 @@ const Loginpage = () => (
       <div className="form">
         {/* Log in form Starts */}
         <div className="login-form ">
-          <h3>Login</h3>
+          <h3>LogIn</h3>
           <div className="form-group">
             <input type="text" placeholder="Email Address*" className="form-control"></input>
           </div>
@@ -30,7 +30,7 @@ const Loginpage = () => (
             </label>
           </div>
             <button type="buttom" className="submit-btn">Login</button>
-            <p><a href="//#region" className="register-btn">Register</a>|<a href="//#region" className="lost-pass-btn">Lost Your Password?</a></p>
+            <p><a href="//#region" className="register-btn">Register |</a> <a href="//#region" className="lost-pass-btn">Lost Your Password?</a></p>
         </div>
       {/* Log in form Ends */}
       {/* Register form Starts */}
@@ -49,23 +49,24 @@ const Loginpage = () => (
             <input type="password" placeholder="Password*" className="form-control"></input>
           </div>
             <button type="buttom" className="submit-btn">Register</button>
-            <p><a href="//#region" className="login-btn">Login</a>|<a href="//#region" className="lost-pass-btn">Lost Your Password?</a></p>
+            <p><a href="//#region" className="login-btn">Login |</a> <a href="//#region" className="lost-pass-btn">Lost Your Password?</a></p>
         </div>
             {/* Registerform Ends */}
             {/* Lost Password form Starts */}
-        <div className="register-form form-hidden">
+        <div className="lost-password-form form-hidden">
           <h3>Lost Your Password?</h3>
           <h5>Check you mailbox to create new password.</h5>
           <div className="form-group">
             <input type="text" placeholder="Email Address*" className="form-control"></input>
           </div>
             <button type="buttom" className="submit-btn">Reset Password</button>
-            <p><a href="//#region" className="login-btn">Login</a>|<a href="//#region" className="register-btn">Register</a></p>
+            <p><a href="//#region" className="login-btn">Login |</a> <a href="//#region" className="register-btn">Register</a></p>
         </div>
-            {/* Registerform Ends */}
+            {/* Lost Password form Ends */}
       </div>
       </div>
     </div>
+    <script src="Statechanger.js"></script>
  </body>
 
 );
--- src/Statechanger.js
+++ src/Statechanger.js
@@ -1,19 +1,33 @@
-const loginBtn = document.querySelectorAll(".login-btn"),
-      registerBtn = document.querySelector(".register-btn"),
-      lostPassBtn = document.querySelector(".lost-pass-btn");
+// const loginBtn = document.querySelectorAll(".login-btn"),
+//       registerBtn = document.querySelector(".register-btn"),
+//       lostPassBtn = document.querySelector(".lost-pass-btn");
+//       box = document.querySelector(".box"),
+//       loginForm = document.querySelector(".login-form"),
+//       registerForm = document.querySelector(".register-form"),
+//       lost = document.querySelector(".lost-password-form");
 
-registerBtn.forEach((btn) => {
-    btn.addEventListener("click",() =>{
-        console.log("hi")
-    })
-}); 
-loginBtn.forEach((btn) => {
-    btn.addEventListener("click",() =>{
-        console.log("hi")
-    })
-}); 
-lostPassBtn.forEach((btn) => {
-    btn.addEventListener("click",() =>{
-        console.log("hi")
-    })
-}); 
+// registerBtn.forEach((btn) => {
+//     btn.addEventListener("click",() =>{
+//        box.classList.add("slide-active");
+//        registerForm.classList.remove("form-hidden");
+//        loginForm.classList.add("form-hidden");
+//        lostPasswordForm.classList.add("form-hidden");
+//     });
+// });
+
+// loginBtn.forEach((btn) => {
+//     btn.addEventListener("click",() =>{
+//        box.classList.remove("slide-active");
+//        registerForm.classList.add("form-hidden");
+//        loginForm.classList.remove("form-hidden");
+//        lostPasswordForm.classList.add("form-hidden");
+//     });
+// });
+
+// loginBtn.forEach((btn) => {
+//     btn.addEventListener("click",() =>{
+//        registerForm.classList.add("form-hidden");
+//        loginForm.classList.add("form-hidden");
+//        lostPasswordForm.classList.remove("form-hidden");
+//     });
+// });

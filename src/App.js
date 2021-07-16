7a27c1bb5f0d604051e7e91284d763b3b89cabef
Shohrukh Jalolov <sjalolov@lclark.edu>
2021-07-16 15:40:46 -0700

    Updated Version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import './index.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import Login from "./components/LoginPage";
